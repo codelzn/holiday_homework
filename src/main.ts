3f32f43db17a2b19944766c421796a18dd56e40a
codelzn <lzn724958732@gmail.com>
2022-08-15 12:25:40 +0900

    complete:３時間で課題終わった！

--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,14 @@
 import './style.css';
-class HomeWork1 {
-  public baseDate: string[];
-  protected inputTag: HTMLInputElement;
-  protected userInput?: string;
-  protected outputTag: HTMLElement;
-  constructor(baseDate: string[]) {
-    this.baseDate = baseDate;
-    // 入力フォームを取得
-    this.inputTag = document.querySelector('.work1')!;
-    this.inputTag.addEventListener('change', this.onChange.bind(this));
-    // 出力要素を取得
-    this.outputTag = document.querySelector('.result1')!;
-  }
-  // ユーザーが入力した日付を取得する
-  protected onChange(): void {
-    this.userInput = this.inputTag.value;
-    this.getResult();
-  }
-
-  // 結果
-  public getResult(): void {
-    // 基準日から経過した日数
-    const passedDays = this.getPassedDays();
-    // 曜日を取得する
-    const yobi = this.getYobi(passedDays);
-    // 結果を出力する
-    const year = this.userInput && this.userInput.split('-')[0];
-    const month = this.userInput && this.userInput.split('-')[1];
-    const day = this.userInput && this.userInput.split('-')[2];
-    yobi
-      ? (this.outputTag.innerHTML = `${year}年${month}月${day}日は${yobi}曜日です`)
-      : (this.outputTag.innerHTML = '正しい年月日を入力してください');
-  }
-
+// 課題１と課題２の共通クラス
+class BaseClass {
   // 閏年かどうかを判定する
-  protected isLeapYear(year: number): boolean {
+  private isLeapYear(year: number): boolean {
     return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
   }
 
   // 今年何日を経過したかを取得する
-  protected getDays(): number {
-    const arr = this.userInput!.split('-');
+  private getDays(nowDate: string): number {
+    const arr = nowDate.split('-');
     let days = Number(arr[2]);
     const month = Number(arr[1]);
     const year = Number(arr[0]);
@@ -59,20 +27,66 @@ class HomeWork1 {
     }
     return days;
   }
+
   // 基準日から経過した日数を取得する
-  protected getPassedDays(): number {
+  protected getPassedDays(startDate: string, endDate: string): number {
     // 何年経った
-    const baseYear = Number(this.baseDate[0].split('-')[0]);
-    const inputYear = Number(this.userInput!.split('-')[0]);
-    const passedYear = Math.abs(inputYear - baseYear) - 1;
-    const passedYearNumber: number[] = [];
-    for (let i = 0; i <= passedYear; i++) {
-      passedYearNumber.push(baseYear + i);
+    const baseYear = Number(startDate.split('-')[0]);
+    const inputYear = Number(endDate.split('-')[0]);
+    const passedYear =
+      Math.abs(inputYear - baseYear) === 0
+        ? 0
+        : Math.abs(inputYear - baseYear) - 1;
+    if (passedYear !== 0) {
+      const passedYearNumber: number[] = [];
+      for (let i = 0; i <= passedYear; i++) {
+        passedYearNumber.push(baseYear + i);
+      }
+      const passedYearDays = passedYearNumber.reduce((acc, cur) => {
+        return acc + (this.isLeapYear(cur) ? 366 : 365);
+      }, 0);
+      return (
+        passedYearDays + this.getDays(endDate) - this.getDays(startDate) + 1
+      );
+    } else {
+      return this.getDays(endDate) - this.getDays(startDate) + 1;
     }
-    const passedYearDays = passedYearNumber.reduce((acc, cur) => {
-      return acc + (this.isLeapYear(cur) ? 366 : 365);
-    }, 0);
-    return passedYearDays + this.getDays();
+  }
+}
+// 課題１
+class HomeWork1 extends BaseClass {
+  public baseDate: string[];
+  public inputTag: HTMLInputElement;
+  public userInput?: string;
+  public outputTag: HTMLElement;
+  constructor(baseDate: string[]) {
+    super();
+    this.baseDate = baseDate;
+    // 入力フォームを取得
+    this.inputTag = document.querySelector('.work1')!;
+    this.inputTag.addEventListener('change', this.onChange.bind(this));
+    // 出力要素を取得
+    this.outputTag = document.querySelector('.result1')!;
+  }
+  // ユーザーが入力した日付を取得する
+  protected onChange(): void {
+    this.userInput = this.inputTag.value;
+    this.getResult();
+  }
+
+  // 結果
+  protected getResult(): void {
+    // 基準日から経過した日数
+    const passedDays = this.getPassedDays(this.baseDate[0], this.userInput!);
+    // 曜日を取得する
+    const yobi = this.getYobi(passedDays);
+    // 結果を出力する
+    const year = this.userInput && this.userInput.split('-')[0];
+    const month = this.userInput && this.userInput.split('-')[1];
+    const day = this.userInput && this.userInput.split('-')[2];
+    yobi
+      ? (this.outputTag.innerHTML = `${year}年${month}月${day}日は${yobi}曜日です`)
+      : (this.outputTag.innerHTML = '正しい年月日を入力してください');
   }
 
   // 曜日を取得する
@@ -87,34 +101,47 @@ class HomeWork1 {
       }
       useryobi.push(baseyobi[index]);
     }
-    // 最後の計算、その日自身を消すために-1する
     const yobi = useryobi[(totalDays - 1) % 7];
     return yobi;
   }
 }
 new HomeWork1(['2000-1-1', '土']);
 
-class HomeWork2 {
+// 課題２
+class HomeWork2 extends BaseClass {
   public startDateInput: HTMLInputElement;
   public endDateInput: HTMLInputElement;
   public btn: HTMLButtonElement;
+  public outputTag: HTMLElement;
   public startDate?: string;
   public endDate?: string;
   constructor() {
+    super();
     this.startDateInput = document.querySelector('.start')!;
     this.endDateInput = document.querySelector('.end')!;
     this.btn = document.querySelector('.btn')!;
+    this.outputTag = document.querySelector('.result2')!;
     this.init();
   }
 
   protected init(): void {
     this.btn.addEventListener('click', this.onClick.bind(this));
+    window.addEventListener('keydown', e => {
+      e.code === 'Enter' && this.onClick();
+    });
   }
 
   protected onClick(): void {
     this.startDate = this.startDateInput.value;
     this.endDate = this.endDateInput.value;
-    // this.getResult();
+    this.getResult();
+  }
+
+  protected getResult(): void {
+    const result = this.getPassedDays(this.startDate!, this.endDate!);
+    result
+      ? (this.outputTag.innerHTML = `${result.toString()}日です。`)
+      : (this.outputTag.innerHTML = '正しい年月日を入力してください');
   }
 }
 new HomeWork2();
